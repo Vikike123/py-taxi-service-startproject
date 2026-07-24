d91c615b46c9008ea1b0f98970714f9e02f0b7b4
Rábai <h591649@stud.u-szeged.hu>
2026-07-24 22:36:07 +0200

    Fix: use settings.AUTH_USER_MODEL for car drivers relation

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from django.db import models
 
 # Create your models here.
 class Manufacturer(models.Model):
@@ -26,7 +28,7 @@ class Driver(AbstractUser):
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(Driver, related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
     class Meta:
         ordering = ["model"]
